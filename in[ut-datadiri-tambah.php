dd7f79366d9e5bbedea3d4d2a9b5c831fa72c974
SinthaWulan <110141476+SinthaWulan@users.noreply.github.com>
2022-09-16 19:23:40 +0700

    Add files via upload

---
+++ in[ut-datadiri-tambah.php
@@ -0,0 +1,44 @@
+<h1>Tambah Data</h1>
+<form action="in[ut-datadiri-tambah.php" method="POST">
+    <label for="nis">Nomor Induk Siswa :</label>
+    <input type="number" name="nis" placeholder="Ex.12333122"/><br>
+
+    <label for="nama">Nama Lengkap :</label>
+    <input type="text" name="nama" placeholder="Ex.Sintha Nur Wulan"/><br>
+
+    <label for="tanggal_lahir">Tanggal Lahir :</label><br>
+    <input type="date" name="tanggal_lahir"/><br>
+
+    <label for="nilai">Nilai:</label><br>
+    <input type="number" name="nilai" placeholder="Ex.80.56"/><br>
+
+<input type="submit" name="simpan" value="Simpan Data"/>
+<a href="input-datadiri.php">Kembali</a>
+</form>
+
+
+<?php
+        if( isset($_POST["simpan"]) ){
+            echo $nis = $_POST["nis"];
+            echo $nama = $_POST["nama"];
+            echo $tanggal_lahir = $_POST["tanggal_lahir"];
+            echo $nilai = $_POST["nilai"];
+
+           //CREATE - Menambahakan Data ke Database
+           $query ="
+                    INSERT INTO datadiri VALUES
+                    ('$nis','$nama','$tanggal_lahir','$nilai');
+            ";
+            include('./input-config.php');
+            $insert = mysqli_query($mysqli, $query);
+
+            if($insert){
+                echo"
+                    <script>
+                    alert('Data berhasil ditambahkan');
+                    window.location='input-datadiri.php';
+                    </script>
+                ";
+            }
+        }
+?>
